84be18a809990894efbe25dde0b73fbd66af9768
ALESSANDRO <106878187+alessandro2-022@users.noreply.github.com>
2025-10-31 00:59:36 -0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,7 @@
+
+// Define a estrutura (o "formato") de um objeto de serviço
+export interface Service {
+  title: string;       // O nome do serviço
+  description: string; // Uma breve descrição do que o serviço inclui
+  price: string;       // O preço ou faixa de preço do serviço
+}
