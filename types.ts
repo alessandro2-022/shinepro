491e1582c312e362754329301ddfa56629f42157
ALESSANDRO <106878187+alessandro2-022@users.noreply.github.com>
2025-10-31 01:07:25 -0300

    Delete types.ts

--- types.ts
+++
@@ -1,7 +0,0 @@
-
-// Define a estrutura (o "formato") de um objeto de serviço
-export interface Service {
-  title: string;       // O nome do serviço
-  description: string; // Uma breve descrição do que o serviço inclui
-  price: string;       // O preço ou faixa de preço do serviço
-}
